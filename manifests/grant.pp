959559b50173dcdc61880880b5307aef86ee6f7e
luksi1 <luke.simmons@vgregion.se>
2016-03-02 14:06:39 +0100

    Changed the name of the exec so that it would be unique on each run.

--- manifests/grant.pp
+++ manifests/grant.pp
@@ -5,7 +5,7 @@ define windows_security_policy::grant ($privilege, $account) {
   $command = "Import-Module UserRights; Grant-UserRight -Privileges ${privilege} -Account ${account}"
   $check = "Import-Module UserRights; Get-UserRight -Account ${account} | Where-Object {\$_.Name -eq \"${privilege}\"}"
   
-  exec { 'grant_windows_security_policy':
+  exec { "${account}-${privilege}":
     command   => $command,
     onlyif    => $check,
     logoutput => true,
